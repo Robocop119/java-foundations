6e3007a3e6b4a79604262013213351f22c2e5e8c
Mario <71067562+Robocop119@users.noreply.github.com>
2026-02-15 23:34:56 +0100

    Update HolaMundo.java

--- OneDrive/Escritorio/Archivos/proyectos-java/HolaMundo.java
+++ OneDrive/Escritorio/Archivos/proyectos-java/HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Iniciando mi camino profesional en Java");
+        System.out.println("Hola Mundo");
     }
 }
